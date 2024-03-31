fd0bdbb130747ac048fca4d5ab05d5df4c0a5aaa
Avi0304 <157276472+Avi0304@users.noreply.github.com>
2024-03-31 21:26:21 +0530

    change2

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -106,7 +106,7 @@ const Menu = () => {
       <div className="menu-page">
       <header className="mt-5">
           <div className="container h-100 d-flex align-items-center justify-content-center">
-            <h1 className="text-light">Book Table</h1>
+            <h1 className="text-light">Menu</h1>
           </div>
         </header>
       </div>
